1bf9ef937fd5c2d41d92eefe03fb7fab2a4bc722
artie <artieleach@gmail.com>
2024-08-16 14:52:36 -0500

    added basic enemies

--- Game/TowerDefense/Bullets/bullet.gd
+++ Game/TowerDefense/Bullets/bullet.gd
@@ -2,11 +2,11 @@ class_name Bullet extends Node2D
 
 @export var direction : float = 0.0
 var speed : float = 10
-var velocity = 0.0
 
 func _ready() -> void:
 	look_at(get_global_mouse_position())
 
 func _process(delta: float) -> void:
 	position += transform.x * speed
-	pass
+	position.distance_to(Vector2(160, 240))
+	
