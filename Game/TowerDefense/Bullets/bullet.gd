e3a7b4b648eee4f19f477746dc0630db9762f17a
artie <artieleach@gmail.com>
2024-08-16 14:08:18 -0500

    bullets added

--- Game/TowerDefense/Bullets/bullet.gd
+++ Game/TowerDefense/Bullets/bullet.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Bullet extends Node2D
 
 @export var direction : float = 0.0
 var speed : float = 10
--- Game/TowerDefense/tower_defense.gd
+++ Game/TowerDefense/tower_defense.gd
@@ -1,9 +1,9 @@
 extends Node
 
-@onready var bullet = preload("res://Game/TowerDefense/Bullets/PeaShooter.tscn")
+@onready var bullet = preload("res://Game/TowerDefense/Bullets/Bullet.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("accept"):
-		var new_bullet : Node2D = bullet.instantiate()
+		var new_bullet : Bullet = bullet.instantiate()
 		new_bullet.position = $Tower.position
 		add_child(new_bullet)
