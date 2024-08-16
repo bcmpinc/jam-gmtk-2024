a54093c264b32010fd2a71c175f300941eac7f73
artie <artieleach@gmail.com>
2024-08-16 14:00:23 -0500

    initial sketch for gameplay

--- Game/TowerDefense/tower_defense.gd
+++ Game/TowerDefense/tower_defense.gd
@@ -1,1 +1,9 @@
 extends Node
+
+@onready var bullet = preload("res://Game/TowerDefense/Bullets/PeaShooter.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("accept"):
+		var new_bullet : Node2D = bullet.instantiate()
+		new_bullet.position = $Tower.position
+		add_child(new_bullet)
