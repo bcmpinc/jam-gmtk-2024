1ad985063dc4172be425187e2e92dbd5ebe23056
Bauke Conijn <bcmpinc@gmail.com>
2024-08-18 01:51:06 +0200

    Enable audio but don't load bad music.

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,11 +1,8 @@
 extends TextureRect
 
-@export_file("*.tscn") var game : String
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.music.stream = load("res://Menu/copyright-free-background-music-216517.mp3")
-	ResourceLoader.load_threaded_request(game, "PackedScene")
+	# Global.music.stream = load("res://Menu/copyright-free-background-music-216517.mp3")
 	
 	$Platform.pressed.connect(func():
 		Global.change_scene_to_file("res://Game/Platformer/World.tscn")
